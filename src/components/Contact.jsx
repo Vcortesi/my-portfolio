e11bfb05add0edc9e7b398ac6663e03b2193d8d0
Vincent Cortesi <vincentcortesi@gmail.com>
2023-02-09 01:27:38 -0600

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ function Contact() {
                   className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                />
                <button
-                  type="submit"
+                  type="button"
                   className="text-center inline-block px-8 py-3 w-max text-base
                    font-medium rounded-md text-white bg-gradient-to-r from-green-600
                    to-blue-500 drop-shadow-md"
