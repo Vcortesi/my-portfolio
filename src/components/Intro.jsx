0b415ba81a852ec9e29f1dd1165a68b0130e9c0d
Vincent Cortesi <vincentcortesi@gmail.com>
2023-02-09 00:32:25 -0600

    first commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Intro() {
+    return(
+        <div className="flex items-center
+        justify-center flex-col text-center pt-20 
+        pb-6">
+            <h1 className ="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Vincent Cortesi</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+            <p className ="text-sm max-w-xl mb-6 font-bold">This is my entire bio</p>
+         </div>
+    )
+}
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title: 'TeemoBot',
+        imgUrl: '/assets/TeemoBot.jpg',
+        stack: ['Python','Discord','Riot API','Insert Hosting Here'],
+        link:'',
+    },
+    {
+        title: 'Brick Breaker',
+        imgUrl: '/assets/BrickBreaker.jpg',
+        stack: ['Java'],
+        link:'',
+    },
+    /*{
+        title: 'Portfolio',
+        imgUrl: 'src/assets/p4.jpg',
+        stack: ['React','Tailwind','Vite'],
+        link:'',
+    },*/
+    {
+        title: 'Attendance Tracker',
+        imgUrl: '/assets/istockphoto-1050712820-612x612.jpg',
+        stack: ['HTML/CSS','Javascript','React','NodeJS','MongoDB'],
+        link:'',
+    },
+]
