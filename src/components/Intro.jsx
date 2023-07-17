c9cdfb301178ec0b01b34890794aa40ae04ac617
Vincent Cortesi <vincentcortesi@gmail.com>
2023-07-17 16:42:47 -0500

    intro update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,9 @@ function Intro() {
         pb-6">
             <h1 className ="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Vincent Cortesi</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className ="text-sm max-w-xl mb-6 font-bold">I am a highly motivated computer science student with a passion for software engineering, video-games, and technology! These projects are built from the ground up from planning and designing to implementing. I am excitedly to launch a career in Software Development, and eagerly preparing for graduation in May 2023!   </p>
+            <p className ="text-sm max-w-xl mb-6 font-bold">I am a highly motivated computer science student with a passion for software engineering,
+             video-games, and technology! These projects are built from the ground up from planning and designing to implementing.
+             I graduated Cum Laude in Computer Science from Lewis University, and I am actively seeking software engineering roles.   </p>
          </div>
     )
 }
